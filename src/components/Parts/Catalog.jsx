34607ecbb0b9cff6db69affe8aa0de94ca42ae95
Kamazaz <134839771+Kamazaz@users.noreply.github.com>
2024-03-03 19:30:24 +0400

    Add files via upload

---
+++ src/components/Parts/Catalog.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Prod_mini from './StackItems/Prod_mini';
+
+function Catalog({prods, err, all, SRVRADDRESS}){
+    return(
+        <>
+            <div className='catalog' id="catalog">
+                {prods.lenght != 0
+                    ?prods.map((prod) =>
+                        <Prod_mini info={prod} key={prod.id} err={err} SRVRADDRESS={SRVRADDRESS}/>
+                    )
+                    :<h2>Подгружаем каталог</h2>
+                }
+            </div>
+            {all != ''
+                ?<div className="after_cat">{all}</div>
+                :<div className="after_cat">Подгружаем интересные товары...</div>
+            }
+        </>
+    )
+}
+
+export default Catalog;
